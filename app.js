e99be64da921e30874b0a5d3f6b904b66a68d78a
Lean Rasmussen <Leanmkrasmussen@gmail.com>
2018-09-24 11:30:34 +0200

    Computer move

--- app.js
+++ app.js
@@ -1,15 +1,4 @@
-$(window).scroll(function(){
-    let wScroll = $(this).scrollTop();
-    let scropp = wScroll*-0.15;
-    let textscroll =  wScroll*1.5;
-    $('.parallax-advanced__background--front').css({
-        "transform":"translate(0px," + scropp +"px)"
-    })
-    $('.parallax-advanced__background--text').css({
-        "transform":"translate(0px," + textscroll +"px)",
-        "opacity": 1- (textscroll*0.002),
-    })
-})
+
 
 //module
 var TMbling= angular.module('TMbling',['ngRoute']);
@@ -39,5 +28,8 @@ TMbling.config(function($routeProvider){
     .when('/SVG', {
         templateUrl: 'pages/svg.html',
     })
+        .when('/input', {
+            templateUrl: 'pages/input.html',
+        })
 
 });
